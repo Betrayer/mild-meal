13b4a9396178d54d8af36bce0684ca8c475592fa
Bohdan Illiakov <bohdani@voiceable.co>
2022-09-11 02:56:34 +0300

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import HomePage from "../pages/homepage/HomePage";
+
+const App: FC = () => {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+};
+
+export default App;
