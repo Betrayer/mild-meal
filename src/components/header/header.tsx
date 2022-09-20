7516885fb28e4791623a97b7f7a72dde597e16ab
Jumpedfox <sosobaka@gmail.com>
2022-09-21 00:04:30 +0300

    changed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -11,14 +11,9 @@ const Header: FC = () => {
     setMenuIsVisible(!menuIsVisible);
   };
 
-  const handleCategoriesClick = () => {
+  const handleCategoriesAndSearchClick = () => {
     setCategoriesAreOpened(!categoriesAreOpened);
     setSearchIsOpened(!searchIsOpened);
-  };
-
-  const handleSearchClick = () => {
-    setSearchIsOpened(!searchIsOpened);
-    setCategoriesAreOpened(!categoriesAreOpened);
   };
 
   const searchAlert = () => {
@@ -29,54 +24,54 @@ const Header: FC = () => {
   };
 
   return (
-    <>
-      <header className="header">
-        <div className="header_logo">
-          <span className="header_logo-name">MILDMEAL</span>
-          <span className="header_logo-tagline">
-            your best kitchen assistant
-          </span>
-        </div>
+    <header className="header">
+      <div className="header_logo">
+        <span className="header_logo-name">mildmeal</span>
+        <span className="header_logo-tagline">your best kitchen assistant</span>
+      </div>
 
+      <div
+        className={`header_menu-button ${
+          menuIsVisible ? "closeMenu" : "openMenu"
+        }`}
+        onClick={handleMenuClick}
+      ></div>
+
+      <div
+        className={`header_categories-button_wrapper wrapper ${
+          categoriesAreOpened ? "openedCategories" : ""
+        }`}
+      >
         <div
-          className={`header_menu-button ${
-            menuIsVisible ? "closeMenu" : "openMenu"
+          className={`header_categories-button ${
+            categoriesAreOpened ? "hiddenElement" : ""
           }`}
-          onClick={handleMenuClick}
+          onClick={
+            !categoriesAreOpened ? handleCategoriesAndSearchClick : undefined
+          }
         ></div>
+      </div>
 
+      <div
+        className={`header_search-form_wrapper wrapper ${
+          searchIsOpened ? "openedSearch" : ""
+        }`}
+      >
         <div
-          className={`header_categories-button_wrapper wrapper ${
-            categoriesAreOpened ? "openedCategories" : ""
-          }`}
-        >
-          <div
-            className={`header_categories-button ${
-              categoriesAreOpened ? "hiddenElement" : ""
-            }`}
-            onClick={!categoriesAreOpened ? handleCategoriesClick : undefined}
-          ></div>
-        </div>
+          className="header_search-form"
+          onClick={
+            searchIsOpened ? searchAlert : handleCategoriesAndSearchClick
+          }
+        ></div>
+      </div>
 
-        <div
-          className={`header_search-form_wrapper wrapper ${
-            searchIsOpened ? "openedSearch" : ""
-          }`}
-        >
-          <div
-            className="header_search-form"
-            onClick={searchIsOpened ? searchAlert : handleSearchClick}
-          ></div>
-        </div>
-
-        <div className="header_login-button_wrapper wrapper">
-          <span className="header_login-button" onClick={loginAlert}>
-            log in
-          </span>
-          <ThemeChangeButton />
-        </div>
-      </header>
-    </>
+      <div className="header_login-button_wrapper wrapper">
+        <span className="header_login-button" onClick={loginAlert}>
+          log in
+        </span>
+        <ThemeChangeButton />
+      </div>
+    </header>
   );
 };
 
