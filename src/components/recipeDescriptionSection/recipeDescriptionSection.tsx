d1a9a50ac0467a8637337ba90059a30184495545
Jumpedfox <sosobaka@gmail.com>
2022-10-14 01:52:38 +0300

    commit to fix another branch

--- src/components/recipeDescriptionSection/recipeDescriptionSection.tsx
+++ src/components/recipeDescriptionSection/recipeDescriptionSection.tsx
@@ -2,9 +2,6 @@ import React, { FC } from "react";
 import "./recipeDescriptionSection.scss";
 
 const RecipeDescriptionSection: FC = () => {
-  const handleClick = () => {
-    alert("clicked!");
-  };
 
   return (
     <section className="recipe-description-section">
@@ -14,12 +11,6 @@ const RecipeDescriptionSection: FC = () => {
           <div className="recipe-description-section__content__info__ingredients"></div>
           <div className="recipe-description-section__content__info__details"></div>
         </div>
-        <span
-          className="recipe-description-section__content__button"
-          onClick={handleClick}
-        >
-          SHOW NUTRITION FACTS
-        </span>
       </div>
     </section>
   );
--- src/components/recipeNutritionsSection/recipeNutritionsSection.tsx
+++ src/components/recipeNutritionsSection/recipeNutritionsSection.tsx
@@ -1,21 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./recipeNutritionsSection.scss";
 
 const RecipeNutritionsSection: FC = () => {
-  const handleClick = () => {
-    alert("clicked!");
+  const [nutritionsPosition, setNnutritionsPosition] = useState("hidden-under");
+  const [butonKeyword, setButtonKeyword] = useState("show")
+
+  const handleNutritionsButton = () => {
+    setNnutritionsPosition(nutritionsPosition ? "" : "hidden-under");
+    setButtonKeyword(butonKeyword === "show" ? "hide" : "show")
+    console.log(butonKeyword)
   };
 
   return (
-    <section className="recipe-nutritions-section">
+    <section className={`recipe-nutritions-section ${nutritionsPosition}`}>
       <div className="recipe-nutritions-section__content">
         <span className="recipe-nutritions-section__content__title"></span>
         <span className="recipe-nutritions-section__content__size"></span>
         <span className="recipe-nutritions-section__content__calories"></span>
         <ul className="recipe-nutritions-section__content__nutritions"></ul>
       </div>
-      <span className="recipe-nutritions-section__button" onClick={handleClick}>
-        SHOW NUTRITION FACTS
+      <span
+        className="recipe-nutritions-section__button"
+        onClick={handleNutritionsButton}
+      >
+        {butonKeyword} NUTRITION FACTS
       </span>
     </section>
   );
