4841e4fd668dd737efef75daa69f9213f4bb4978
Jumpedfox <sosobaka@gmail.com>
2022-10-17 22:10:05 +0300

    added recipe rendering from store + stule fixes

--- src/components/recipeCard/recipeCard.tsx
+++ src/components/recipeCard/recipeCard.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from "react";
 import { useNavigate } from "react-router-dom";
 import "./recipeCard.scss";
+import gluten from "../../assets/images/gluten.png";
+import gluten_free from "../../assets/images/gluten-free.png";
+import vegan from "../../assets/images/vegan.png";
+import non_vegan from "../../assets/images/non-vegan.png";
+import cooking_time from "../../assets/images/cooking-time2.png";
 
-const RecipeCard: FC = () => {
+interface RecipeCardProps {
+  recipe: any;
+}
+
+const RecipeCard: FC<RecipeCardProps> = ({ recipe }) => {
   const navigate = useNavigate();
 
   const recipeRedirect = (): void => {
@@ -10,23 +19,28 @@ const RecipeCard: FC = () => {
   };
 
   return (
-    <div className="recipe-card" onClick={recipeRedirect}>
-      <div className="recipe-card__upper-part">
-        <div className="recipe-card__upper-part__picture"></div>
-        <div className="recipe-card__upper-part__points-and-name">
-          <div className="recipe-card__upper-part__points-and-name__points"></div>
-          <span className="recipe-card__upper-part__points-and-name__name">
-            rice with richmond rice and rice water with rice butter
-          </span>
-        </div>
-      </div>
-      <div className="recipe-card__lower-part">
-        <span className="recipe-card__lower-part__description">
-          rice gently covered in rice coat made of crunchy rice andrice rice
-          rice
-        </span>
-      </div>
-    </div>
+    <li className="recipe-card" onClick={recipeRedirect} key={recipe.id}>
+      <img
+        className="recipe-card__picture"
+        src={recipe.image}
+        alt={recipe.title}
+      />
+      <img className="recipe-card__time" src={cooking_time} alt="time" />
+      <span className="recipe-card__time-num">{recipe.readyInMinutes}</span>
+
+      {recipe.glutenFree ? (
+        <img className="recipe-card__gluten" src={gluten} alt="gluten" />
+      ) : (
+        <img className="recipe-card__gluten" src={gluten_free} alt="gluten" />
+      )}
+
+      {recipe.vegan ? (
+        <img className="recipe-card__vegan" src={vegan} alt={recipe.vegan} />
+      ) : (
+        <img className="recipe-card__vegan" src={non_vegan} alt="vegan" />
+      )}
+      <span className="recipe-card__description">{recipe.title}</span>
+    </li>
   );
 };
 
--- src/components/trendingSection/trendingSection.tsx
+++ src/components/trendingSection/trendingSection.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { getRecipes } from "../../redux/actions/recipes";
 import { RootState } from "../../redux/rootReducer";
 import RecipeCard from "../recipeCard/recipeCard";
 import "./trendingSection.scss";
@@ -9,27 +11,32 @@ const TrendingSection: FC = () => {
     (state: RootState) => state.global.themeColor
   );
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch(getRecipes());
+  }, []);
+
+  const randomRecipes = useSelector(
+    (state: RootState) => state.recipes.randomRecipes
+  );
+  console.log(randomRecipes);
+  console.log(randomRecipes.slice(0, 2));
+
   return (
     <section className={`trending-section ${currentThemeColor}`}>
-      <span className="trending-section__title">trending recipes</span>
-      <div className="trending-section__content-block">
-        {window.innerWidth < 1024 && window.innerWidth >= 768 ? (
-          <>
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-          </>
-        ) : (
-          <>
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-          </>
-        )}
-      </div>
+      {/* <span className="trending-section__title">trending recipes</span> */}
+      <ul className="trending-section__content-block">
+        {window.innerWidth >= 768 && window.innerWidth < 1024
+          ? randomRecipes
+              .slice(0, 3)
+              .map((recipe: any) => <RecipeCard recipe={recipe} />)
+          : window.innerWidth >= 1024
+          ? randomRecipes
+              .slice(0, 4)
+              .map((recipe: any) => <RecipeCard recipe={recipe} />)
+          : randomRecipes.map((recipe: any) => <RecipeCard recipe={recipe} />)}
+      </ul>
     </section>
   );
 };
