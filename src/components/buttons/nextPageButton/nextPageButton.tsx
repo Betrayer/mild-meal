de3e8744645170d4816feca4378640f3d478651b
Betrayer <stardestroyer@ukr.net>
2022-10-23 07:44:27 +0100

    fixed functional component

--- src/components/buttons/nextPageButton/nextPageButton.tsx
+++ src/components/buttons/nextPageButton/nextPageButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import "./nextPageButton.scss";
 
@@ -7,10 +7,10 @@ const handleClick = (e: React.MouseEvent<HTMLInputElement>) => {
     // alert ("next page!")
 }
 
-const NextPageButton = () => {
+const NextPageButton: FC = () => {
     return (
         <div className='next-page-button' onClick={handleClick}>
-            <div className='next-page-button__upper-part'/>
+            <div className='next-page-button__upper-part' />
             <span className='next-page-button__lower-part'>show next page</span>
         </div>
     );
