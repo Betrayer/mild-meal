10db2c32e129f2a68f4baea6c32409881ff9af18
Betrayer <stardestroyer@ukr.net>
2022-11-05 20:48:07 +0200

    added custom collection on register

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,113 +1,134 @@
-import React, { FC, Suspense, useEffect, useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../../redux/rootReducer";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../configs/firebase.config";
-import { getRecipes } from "../../redux/actions/recipes";
-import Header from "../header/header";
-import LoginAndRegistrationSection from "../sections/loginAndRegistrationSection/loginAndRegistrationSection";
-import "./index.scss";
-import NotFoundPage from "../../pages/404Page/404Page";
+import React, { FC, Suspense, useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../../redux/rootReducer';
+import {
+	collection,
+	getDocs,
+	setDoc,
+	doc,
+	updateDoc,
+	arrayUnion,
+} from 'firebase/firestore';
+import { db } from '../../configs/firebase.config';
+import { getRecipes } from '../../redux/actions/recipes';
+import Header from '../header/header';
+import LoginAndRegistrationSection from '../sections/loginAndRegistrationSection/loginAndRegistrationSection';
+import './index.scss';
+import NotFoundPage from '../../pages/404Page/404Page';
 
 const App: FC = () => {
-  const HomePage = React.lazy(() => import("../../pages/homepage/HomePage"));
-  const RecipePage = React.lazy(
-    () => import("../../pages/recipePage/recipePage")
-  );
-  const SearchResultsPage = React.lazy(
-    () => import("../../pages/searchResultsPage/searchResultsPage")
-  );
-  const ProfilePage = React.lazy(
-    () => import("../../pages/profilePage/profilePage")
-  );
+	const HomePage = React.lazy(() => import('../../pages/homepage/HomePage'));
+	const RecipePage = React.lazy(
+		() => import('../../pages/recipePage/recipePage')
+	);
+	const SearchResultsPage = React.lazy(
+		() => import('../../pages/searchResultsPage/searchResultsPage')
+	);
+	const ProfilePage = React.lazy(
+		() => import('../../pages/profilePage/profilePage')
+	);
 
-  // const getDummyData = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "test"));
-  //   querySnapshot.forEach((doc) => {
-  //     console.log(doc.id, " => ", doc.data());
-  //   });
-  // };
+	// const getDummyData = async () => {
+	// 	const querySnapshot = await getDocs(collection(db, 'test'));
+	// 	querySnapshot.forEach((doc) => {
+	// 		console.log(doc.id, ' => ', doc.data());
+	// 	});
+	// };
 
-  const [showLoginModal, setShowLoginModal] = useState(false);
+	// const data = {
+	// 	favorites: ['qwer'],
+	// 	saved: ['qwer'],
+	// 	todo: ['qwer'],
+	// };
 
-  const dispatch = useDispatch();
+	// const setDummyData = async () => {
+	// 	await setDoc(doc(db, 'users', '0234'), data);
+	// };
 
-  const handleLoginButton = () => {
-    setShowLoginModal(!showLoginModal);
-  };
+	const [showLoginModal, setShowLoginModal] = useState(false);
 
-  const currentThemeColor = useSelector(
-    (state: RootState) => state.global.themeColor
-  );
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    // dispatch(getRecipes());
-  }, []);
+	const handleLoginButton = () => {
+		setShowLoginModal(!showLoginModal);
+	};
 
-  return (
-    <div className={`appWrapper ${currentThemeColor}`}>
-      {window.innerWidth >= 1024 && (
-        <>
-          <div className="decoration-1" />
-          <div className="decoration-2" />
-          <div className="decoration-3" />
-          {currentThemeColor === "bright" ? (
-            <div className="decoration-4 bright" />
-          ) : (
-            <div className="decoration-4 dark" />
-          )}
-        </>
-      )}
+	const currentThemeColor = useSelector(
+		(state: RootState) => state.global.themeColor
+	);
 
-      <Header handleLoginButton={handleLoginButton} />
-      {showLoginModal && (
-        <LoginAndRegistrationSection handleLoginButton={handleLoginButton} />
-      )}
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<>...</>}>
-              <HomePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/recipe/*"
-          element={
-            <Suspense fallback={<>...</>}>
-              <RecipePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/results"
-          element={
-            <Suspense fallback={<>...</>}>
-              <SearchResultsPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <Suspense fallback={<>...</>}>
-              <ProfilePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <Suspense fallback={<>...</>}>
-              <NotFoundPage />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </div>
-  );
+	useEffect(() => {
+		// dispatch(getRecipes());
+		// getDummyData();
+		// setDummyData();
+	}, []);
+
+	return (
+		<div className={`appWrapper ${currentThemeColor}`}>
+			{window.innerWidth >= 1024 && (
+				<>
+					<div className="decoration-1" />
+					<div className="decoration-2" />
+					<div className="decoration-3" />
+					{currentThemeColor === 'bright' ? (
+						<div className="decoration-4 bright" />
+					) : (
+						<div className="decoration-4 dark" />
+					)}
+				</>
+			)}
+
+			<Header handleLoginButton={handleLoginButton} />
+			{showLoginModal && (
+				<LoginAndRegistrationSection
+					handleLoginButton={handleLoginButton}
+				/>
+			)}
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Suspense fallback={<>...</>}>
+							<HomePage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/recipe/*"
+					element={
+						<Suspense fallback={<>...</>}>
+							<RecipePage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/results"
+					element={
+						<Suspense fallback={<>...</>}>
+							<SearchResultsPage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="/profile"
+					element={
+						<Suspense fallback={<>...</>}>
+							<ProfilePage />
+						</Suspense>
+					}
+				/>
+				<Route
+					path="*"
+					element={
+						<Suspense fallback={<>...</>}>
+							<NotFoundPage />
+						</Suspense>
+					}
+				/>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
--- src/components/sections/loginAndRegistrationSection/loginAndRegistrationSection.tsx
+++ src/components/sections/loginAndRegistrationSection/loginAndRegistrationSection.tsx
@@ -1,126 +1,142 @@
 import React, {
-  FC,
-  FormEvent,
-  MutableRefObject,
-  useRef,
-  useState,
-} from "react";
-import { useDispatch } from "react-redux";
+	FC,
+	FormEvent,
+	MutableRefObject,
+	useRef,
+	useState,
+} from 'react';
+import { useDispatch } from 'react-redux';
 import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
-import RegistrationForm from "../../forms/registrationForm/registrationForm";
-import LoginForm from "../../forms/loginForm/loginForm";
-import { app } from "../../../configs/firebase.config";
-import { AuthData } from "../../../types/types";
-import { loginResponce, registerResponce, isAuth } from "../../../redux/actions/auth";
+	getAuth,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import RegistrationForm from '../../forms/registrationForm/registrationForm';
+import LoginForm from '../../forms/loginForm/loginForm';
+import { app, db } from '../../../configs/firebase.config';
+import { AuthData } from '../../../types/types';
+import {
+	loginResponce,
+	registerResponce,
+	isAuth,
+} from '../../../redux/actions/auth';
 
-import "./loginAndRegistrationSection.scss";
+import './loginAndRegistrationSection.scss';
+import { doc, setDoc } from 'firebase/firestore';
 
 interface LoginAndRegistrationProps {
-  handleLoginButton: () => void;
+	handleLoginButton: () => void;
 }
 
 const LoginAndRegistrationSection: FC<LoginAndRegistrationProps> = ({
-  handleLoginButton,
+	handleLoginButton,
 }) => {
-  const [formSwitcher, setFormSwitcher] = useState(true);
+	const [formSwitcher, setFormSwitcher] = useState(true);
 
-  const [authCredentials, setAuthCredentials] = useState<AuthData>({
-    email: "",
-    password: "",
-    passwordConfirmation: "",
-  });
+	const [authCredentials, setAuthCredentials] = useState<AuthData>({
+		email: '',
+		password: '',
+		passwordConfirmation: '',
+	});
 
-  const switchForm = (): void => {
-    setFormSwitcher(!formSwitcher);
-  };
+	const customData = {
+		favorites: [],
+		saved: [],
+		todo: [],
+	};
 
-  const auth = getAuth(app);
-  const sectionRef = useRef(null);
-  const dispatch = useDispatch();
+	const setCustomData = async (id: string): Promise<any> => {
+		await setDoc(doc(db, 'users', id), customData);
+	};
 
-  const handleLoginFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(
-      auth,
-      authCredentials.email,
-      authCredentials.password, 
-    )
-      .then((userCredential) => {
-        const user: any = userCredential.user;
-        dispatch(loginResponce(user.accessToken));
-        dispatch(isAuth(true));
-        handleLoginButton();
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log("ERROR:", errorCode, "ISSUE:", errorMessage);
-      });
-  };
+	const switchForm = (): void => {
+		setFormSwitcher(!formSwitcher);
+	};
 
-  const handleRegistrationFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    authCredentials.passwordConfirmation === authCredentials.password &&
-      createUserWithEmailAndPassword(
-        auth,
-        authCredentials.email,
-        authCredentials.password
-      )
-        .then((userCredential) => {
-          const user: any = userCredential.user;
-          dispatch(registerResponce(user.accessToken));
-          handleLoginButton();
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          console.log("ERROR:", errorCode, "ISSUE:", errorMessage);
-        });
-  };
+	const auth = getAuth(app);
+	const sectionRef = useRef(null);
+	const dispatch = useDispatch();
 
-  const handleAuthInput = (e: FormEvent<HTMLInputElement>): void => {
-    setAuthCredentials({
-      ...authCredentials,
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  };
+	const handleLoginFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		signInWithEmailAndPassword(
+			auth,
+			authCredentials.email,
+			authCredentials.password
+		)
+			.then((userCredential) => {
+				const user: any = userCredential.user;
+				dispatch(loginResponce(user.accessToken));
+				dispatch(isAuth(true));
+				handleLoginButton();
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				console.log('ERROR:', errorCode, 'ISSUE:', errorMessage);
+			});
+	};
 
-  const handleOutsideClick = (
-    ref: MutableRefObject<null>,
-    event: React.MouseEvent<HTMLElement, MouseEvent>
-  ): void => {
-    ref.current === event.target && handleLoginButton();
-  };
+	const handleRegistrationFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		authCredentials.passwordConfirmation === authCredentials.password &&
+			createUserWithEmailAndPassword(
+				auth,
+				authCredentials.email,
+				authCredentials.password
+			)
+				.then((userCredential) => {
+					const user: any = userCredential.user;
+					setCustomData(user.uid);
+					dispatch(registerResponce(user.accessToken));
+					handleLoginButton();
+				})
+				.catch((error) => {
+					const errorCode = error.code;
+					const errorMessage = error.message;
+					console.log('ERROR:', errorCode, 'ISSUE:', errorMessage);
+				});
+	};
 
-  return (
-    <section
-      ref={sectionRef}
-      onClick={(event) => handleOutsideClick(sectionRef, event)}
-      className="login-and-registration-section"
-    >
-      {formSwitcher ? (
-        <LoginForm
-          switchForm={switchForm}
-          handleLoginButton={handleLoginButton}
-          handleLoginFormSubmit={handleLoginFormSubmit}
-          handleAuthInput={handleAuthInput}
-          authCredentials={authCredentials}
-        />
-      ) : (
-        <RegistrationForm
-          switchForm={switchForm}
-          handleRegistrationFormSubmit={handleRegistrationFormSubmit}
-          handleLoginButton={handleLoginButton}
-          handleAuthInput={handleAuthInput}
-          authCredentials={authCredentials}
-        />
-      )}
-    </section>
-  );
+	const handleAuthInput = (e: FormEvent<HTMLInputElement>): void => {
+		setAuthCredentials({
+			...authCredentials,
+			[e.currentTarget.name]: e.currentTarget.value,
+		});
+	};
+
+	const handleOutsideClick = (
+		ref: MutableRefObject<null>,
+		event: React.MouseEvent<HTMLElement, MouseEvent>
+	): void => {
+		ref.current === event.target && handleLoginButton();
+	};
+
+	return (
+		<section
+			ref={sectionRef}
+			onClick={(event) => handleOutsideClick(sectionRef, event)}
+			className="login-and-registration-section"
+		>
+			{formSwitcher ? (
+				<LoginForm
+					switchForm={switchForm}
+					handleLoginButton={handleLoginButton}
+					handleLoginFormSubmit={handleLoginFormSubmit}
+					handleAuthInput={handleAuthInput}
+					authCredentials={authCredentials}
+				/>
+			) : (
+				<RegistrationForm
+					switchForm={switchForm}
+					handleRegistrationFormSubmit={handleRegistrationFormSubmit}
+					handleLoginButton={handleLoginButton}
+					handleAuthInput={handleAuthInput}
+					authCredentials={authCredentials}
+				/>
+			)}
+		</section>
+	);
 };
 
 export default LoginAndRegistrationSection;
